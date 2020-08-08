a6c32a4924e0408f44c4fd213eea14dd0506c5e3
Oleg Eterevsky <oleg@eterevsky.com>
2020-08-08 22:11:16 +0200

    Fix lint warning.

--- examples/layers_layout.py
+++ examples/layers_layout.py
@@ -1,20 +1,20 @@
 import pyglet
 from vui import RootLayout, HStackLayout, VStackLayout, View, LayersLayout
 
-from colors import *
+import color
 
 window = pyglet.window.Window(resizable=True)
 ui = RootLayout(
     window,
     LayersLayout(
-        View(background_color=BLACK),
+        View(background_color=colors.BLACK),
         HStackLayout(
             VStackLayout(View(min_height=100, flex_height=False),
-                        View(background_color=RED),
+                        View(background_color=colors.RED),
                         min_width=200,
                         flex_width=False),
             VStackLayout(
-                View(background_color=TEAL),
+                View(background_color=colors.TEAL),
                 View(),
             ))))
 
--- examples/stack_layout.py
+++ examples/stack_layout.py
@@ -1,21 +1,21 @@
 import pyglet
 from vui import RootLayout, HStackLayout, VStackLayout, View
 
-from colors import *
+import colors
 
 window = pyglet.window.Window(resizable=True)
 ui = RootLayout(
     window,
     HStackLayout(
-        VStackLayout(View(background_color=BLACK,
+        VStackLayout(View(background_color=colors.BLACK,
                           min_height=100,
                           flex_height=False),
-                     View(background_color=RED),
+                     View(background_color=colors.RED),
                      min_width=200,
                      flex_width=False),
         VStackLayout(
-            View(background_color=TEAL),
-            View(background_color=SAND),
+            View(background_color=colors.TEAL),
+            View(background_color=colors.SAND),
         )))
 
 pyglet.app.run()
